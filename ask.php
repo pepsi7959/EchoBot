17b192541dedbebebf29cea3bc6be4c1e2bce097
Narongsak Mala <pepsi_kmutnb@windowslive.com>
2017-07-07 10:32:48 +0700

    Update ask.php

--- ask.php
+++ ask.php
@@ -1,7 +1,7 @@
 <?php
 	$question = $_GET['question'];
-	$url = "http://ask.pannous.com/api?input=".$question."&locale=en_US&timeZone=420&login=test-user&ip=fe80:0:0:0:9068:1693:742:472&botid=0&key=guest&exclude=Dialogues,ChatBot&out=json&clientFeatures=show-images,reminder,say&debug=true";
-	$headers = array('Content-Type: application/json');
+	$url = "http://ask.pannous.com/api?input=".urlencode($question)."&locale=en_US&timeZone=420&login=test-user&ip=fe80:0:0:0:9068:1693:742:472&botid=0&key=guest&exclude=Dialogues,ChatBot&out=json&clientFeatures=show-images,reminder,say&debug=true";
+	$headers = array('Content-Type: application/json','Connection: keep-alive','Connection: close');
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -9,8 +9,11 @@
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 	$result = curl_exec($ch);
 	curl_close($ch);
-	//echo $result . "\r\n";
 	$ret_result=json_decode($result, true);
 	//echo "---->\r\n\r\n".$ret_result['output'][0]['actions']['say']['text']; 
-	echo $ret_result['output'][0]['actions']['say']['text'];
+	if( $ret_result == NULL ){
+		echo "I have no answer.";
+	}else{
+		echo $ret_result['output'][0]['actions']['say']['text'];
+	}
 ?>
